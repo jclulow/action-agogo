504bbe826348fdc2be6aee88fe9121f5b90289c3
Joshua M. Clulow <josh@sysmgr.org>
2020-12-12 00:01:05 -0800

    use arg, dont fail

--- tools/src/main.rs
+++ tools/src/main.rs
@@ -1,10 +1,11 @@
 use std::process::exit;
 
 fn main() {
-    println!("WELCOME TO ACTION AGOGO, VERSION 1!");
+    println!("WELCOME TO ACTION AGOGO, VERSION 1! COLOUR: {}",
+        std::env::args().nth(1).as_deref().unwrap_or("none?!"));
     println!("ENVIRONMENT:");
     for (k, v) in std::env::vars() {
         println!("{} = \"{}\"", k, v);
     }
-    exit(1);
+    exit(0);
 }
