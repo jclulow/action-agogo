d8fd424b7011e7a75cdc736e8c70b9437533888f
Joshua M. Clulow <josh@sysmgr.org>
2020-12-12 00:14:36 -0800

    accept a script

--- tools/src/main.rs
+++ tools/src/main.rs
@@ -1,11 +1,17 @@
 use std::process::exit;
+use anyhow::{Context, Result};
 
-fn main() {
+fn main() -> Result<()> {
     println!("WELCOME TO ACTION AGOGO, VERSION 1! COLOUR: {}",
         std::env::args().nth(1).as_deref().unwrap_or("none?!"));
     println!("ENVIRONMENT:");
     for (k, v) in std::env::vars() {
         println!("{} = \"{}\"", k, v);
     }
+
+    let script = std::env::var("INPUT_SCRIPT").context("read INPUT_SCRIPT")?;
+
+    println!("script: {:#?}", script.lines().collect::<Vec<_>>());
+
     exit(0);
 }
